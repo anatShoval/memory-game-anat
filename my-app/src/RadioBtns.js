de973eaac1b7a15c17dc1f3b603eb14fa9ce0458
Anat Knebel Shoval <anat.shoval@gmail.com>
2017-11-29 15:37:03 +0200

    css changes and defaultChecked

--- my-app/src/RadioBtns.js
+++ my-app/src/RadioBtns.js
@@ -3,7 +3,7 @@ import React from 'react';
 function RadioBtns(props){   
         return (
             <div className="RadioBtnDiv">
-               <RadioBtn value = {4}onChange = {() => props.onChange(4)} />
+               <RadioBtn value = {4} onChange = {() => props.onChange(4)} />
                <RadioBtn value = {6} onChange = {() => props.onChange(6)} />
                <RadioBtn value = {8} onChange = {() => props.onChange(8)} />            
             </div>
@@ -14,7 +14,8 @@ function RadioBtn(props){
     return (
         <div className="radio">
             <label>
-                <input type="radio" name='radio' value={props.value} onChange = {props.onChange} />
+            {props.value===6 ? <input type="radio" name='radio' value={props.value} onChange = {props.onChange} defaultChecked/> : 
+            <input type="radio" name='radio' value={props.value} onChange = {props.onChange}/>}       
             Board {props.value} * {props.value}
             </label>
         </div>
